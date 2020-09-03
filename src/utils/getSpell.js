d989109a769e30a4c31ea981b60d86ff7e3390a3
JonathanSloan <jonathanssloan@protonmail.com>
2020-09-03 02:54:54 -0400

    Spells not correctly load with names containing apostropes

--- src/utils/getSpell.js
+++ src/utils/getSpell.js
@@ -1,6 +1,7 @@
 const getSpellData = (requestedSpell) => {
-  // reduce args array into hyphenated spell name
-  const spellName = requestedSpell.reduce((acc, val) => `${acc}-${val}`);
+  const spellName = requestedSpell.reduce(
+    (acc, val) => `${acc.replace("'", "-")}-${val}`
+  );
 
   // create url string based on spellName
   return `https://www.d20pfsrd.com/magic/all-spells/${spellName[0]}/${spellName}/`;
